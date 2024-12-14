ba3a12da9877f27f7015d95a7128187882c785c5
zoo6x <zoo6x@proton.me>
2024-12-14 20:16:03 +0200

    Testbench for generated assembly

---
+++ test.asm
@@ -0,0 +1,41 @@
+.intel_syntax	noprefix
+
+	.global	_start
+
+	.equ	r0, rax
+	.equ	r1, rcx
+	.equ	r2, rdx
+	.equ	r3, rbx
+	.equ	r5, rbp
+	.equ	r6, rsi
+	.equ	r7, rdi
+
+.text
+
+_start:
+	mov	r0, 1
+
+	mov	r1, 0 
+	mov	r2, 1 
+	mov	r3, 2 
+
+
+8:
+	add	r3, r0 
+
+
+	mov	r5, r3 	
+	mov	r3, r2 	
+	mov	r2, r1 	
+	mov	r1, r5 	
+
+	dec	r0 	
+	jnz	8b
+
+	add	r1, r3 
+	add	r2, r1 
+
+	mov	rdi, r2
+	mov	rax, 60
+_bye:
+	syscall
