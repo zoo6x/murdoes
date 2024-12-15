679744a6122b2b07ca67774aa48312cada7b9be7
zoo6x <zoo6x@proton.me>
2024-12-15 16:15:39 +0200

    An example

--- test.asm
+++ test.asm
@@ -13,29 +13,24 @@
 .text
 
 _start:
-	mov	r0, 5
+	mov	r0, 0
 
+	mov	r1, 11 
+	mov	r2, 22 
+	mov	r3, 33 
 
-	mov	r1, 0 
-	mov	r2, 1 
-	mov	r3, 2 
+	test	r0, r0 
+	jz 	1f
 
-9:
-
-	add	r3, r0 
-
-	mov	r5, r3 
+	mov	r0, r3 
 	mov	r3, r2 
 	mov	r2, r1 
-	mov	r1, r5 
-	dec	r0 
-	jnz	9b
-
-	add	r2, r1 
+	mov	r1, r0 
+1:
+	sub	r2, r1 
 	add	r3, r2 
 
-
-	mov	rdi, r3
+	mov	rdi, r1
 	mov	rax, 60
 _bye:
 	syscall
