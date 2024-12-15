08dbb54b54fc8db9f824bae3eaf01b3fb0f61358
zoo6x <zoo6x@proton.me>
2024-12-15 12:49:18 +0200

    Checked an example cs-for1

--- test.asm
+++ test.asm
@@ -13,24 +13,29 @@
 .text
 
 _start:
-	mov	r0, rdi
+	mov	r0, 5
 
-	test	r0, r0 
-	jz 	11f 
+
 	mov	r1, 0 
 	mov	r2, 1 
+	mov	r3, 2 
 
 9:
-	add	r2, r1 
 
-	mov	r3, r2 	
-	mov	r2, r1 	
-	mov	r1, r3 	
+	add	r3, r0 
 
-	dec	r0 	
+	mov	r5, r3 
+	mov	r3, r2 
+	mov	r2, r1 
+	mov	r1, r5 
+	dec	r0 
 	jnz	9b
-11:
-	mov	rdi, r2
+
+	add	r2, r1 
+	add	r3, r2 
+
+
+	mov	rdi, r3
 	mov	rax, 60
 _bye:
 	syscall
